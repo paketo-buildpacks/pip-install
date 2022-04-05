9db6ffe4eda76df400b491b625302e8af004f99e
Joshua T. Casey <joshuatcasey@gmail.com>
2022-04-05 11:43:42 -0500

    Run "go generate" with latest go and faux versions

--- fakes/install_process.go
+++ fakes/install_process.go
@@ -4,7 +4,7 @@ import "sync"
 
 type InstallProcess struct {
 	ExecuteCall struct {
-		sync.Mutex
+		mutex     sync.Mutex
 		CallCount int
 		Receives  struct {
 			WorkingDir string
@@ -19,8 +19,8 @@ type InstallProcess struct {
 }
 
 func (f *InstallProcess) Execute(param1 string, param2 string, param3 string) error {
-	f.ExecuteCall.Lock()
-	defer f.ExecuteCall.Unlock()
+	f.ExecuteCall.mutex.Lock()
+	defer f.ExecuteCall.mutex.Unlock()
 	f.ExecuteCall.CallCount++
 	f.ExecuteCall.Receives.WorkingDir = param1
 	f.ExecuteCall.Receives.TargetDir = param2
