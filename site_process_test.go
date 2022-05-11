bca9934c32ff7cc938a818ed44921e82592bb01b
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-11 11:15:37 -0400

    Remove deprecated ioutil package.

--- site_process_test.go
+++ site_process_test.go
@@ -3,7 +3,6 @@ package pipinstall_test
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
@@ -28,7 +27,7 @@ func testSiteProcess(t *testing.T, context spec.G, it spec.S) {
 
 	it.Before(func() {
 		var err error
-		layerPath, err = ioutil.TempDir("", "layer")
+		layerPath, err = os.MkdirTemp("", "layer")
 		Expect(err).NotTo(HaveOccurred())
 
 		executable = &fakes.Executable{}
