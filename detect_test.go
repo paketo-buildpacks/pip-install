1bc3e9ee811921d2936a89561adb05419ddc9bc9
Forest Eckhardt <feckhardt@pivotal.io>
2023-05-02 16:17:17 +0000

    Adds a message to packit.Fail in detect

--- detect_test.go
+++ detect_test.go
@@ -65,7 +65,7 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 				_, err := detect(packit.DetectContext{
 					WorkingDir: workingDir,
 				})
-				Expect(err).To(MatchError(packit.Fail))
+				Expect(err).To(MatchError(packit.Fail.WithMessage("no 'requirements.txt' found")))
 			})
 		})
 
